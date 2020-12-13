a8ba1e9e3703e30f1693106855be771782a3339e
Zihong Lin <harrylin317@gmail.com>
2020-12-13 21:55:52 +0800

    watson response message

--- src/main/java/watsonHandler/WatsonHandlerImp.java
+++ src/main/java/watsonHandler/WatsonHandlerImp.java
@@ -1,12 +1,5 @@
 package watsonHandler;
 
-import java.util.Optional;
-import java.util.function.Consumer;
-
 public class WatsonHandlerImp implements WatsonHandler{
 
-    @Override
-    public Optional<String> assistantMessage(String input, String sessionID, Consumer<String> updateSessionID) {
-        return Optional.of("not implemented yet");
-    }
 }
